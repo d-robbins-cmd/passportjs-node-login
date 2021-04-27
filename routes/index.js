42ddd9edda525fe7205f54b83eaf2f274528599c
d-robbins-cmd <derlrobbins@gmail.com>
2021-04-27 17:03:31 -0400

    register submit/flash messages/etc

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const session = require('express-session')
 const router = express.Router()
 const validateRegister = require('../validations/register')
 const validateLogin = require('../validations/login.js')
 router.use( express.urlencoded( { extended: false } ) )
 router.use( express.json() )
 const flash = require('connect-flash')
+router.use( session({
+    secret: 'keyboardcat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+router.use( flash() )
 
-//basic page GETss
+//basic page GET
 router.get('/', ( req, res ) => {
     res.render( 'home' )
 })
@@ -16,15 +24,29 @@ router.get( '/home', ( req, res ) => {
 router.get( '/login', ( req, res ) => {
     res.render( 'login' )
 })
-router.get( '/register', ( req, res ) => {
-    res.render( 'register' )
-})
 
+
+
+router.get( '/register', ( req, res ) => {
+    res.render('register')
+})
 
 router.post( '/register', ( req, res ) => {
     let result = validateRegister( req.body )
     if ( !result.isValid ){
+        req.flash( 'errors', result.errors )  
 
+        //send the form values back to the form
+        const formValues = {
+            name: req.body.name, 
+            email: req.body.email, 
+            password: req.body.password, 
+            password2: req.body.password2
+        }
+        req.flash( 'formValues', formValues )
+
+        res.locals.message = req.flash()
+        res.render('register')
     }
 })
 
@@ -34,8 +56,5 @@ router.post('/login', ( req, res ) => {
     let result = validateLogin( req.body )
     console.log('result', result )
 })
-
-
-
 
 module.exports = router
