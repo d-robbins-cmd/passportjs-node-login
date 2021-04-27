2a1e98898c9d3286d1829ffc564738e02dd9d4d6
d-robbins-cmd <derlrobbins@gmail.com>
2021-04-27 11:24:32 -0400

    adds mongoose stuff

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+require('dotenv').config()
 const express = require( 'express' )
 const app = express()
 const port = process.env.PORT || 5000
 const routes = require( './routes/index' )
 const path = require( 'path' )
 app.set( 'view engine', 'ejs' )
-
-
 app.use( '/', routes )
 app.set( 'views', path.join( __dirname, 'views' ) )
-
+const mongoose = require('mongoose')
+mongoose.connect( process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true } );
+const db = mongoose.connection
+db.once('open', function() {
+    console.log('db connected')
+})
 
 
 
